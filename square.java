fb1d67e03265fc46f17315f2d986441612a1873a
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-11-29 10:12:28 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// update your code below this line
+		Scanner S=new Scanner(System.in);
+		int x=S.nextInt();
+		System.out.println(x*x);
+	}
+}
